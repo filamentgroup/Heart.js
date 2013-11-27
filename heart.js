f742329f2e242cfea76aa6f8f14e0ea0d1e92b18
Mat Marquis <mat@matmarquis.com>
2013-11-27 12:47:02 -0500

    Adds prototype of translateX scolling method, currently qualified by 3D transform support. Adds rAF with setInterval fallback.

--- heart.js
+++ heart.js
@@ -1,5 +1,5 @@
 (function() {
-  var heart, proto;
+  var heart, proto, transform3d, raf = "requestAnimationFrame" in window;
 
   heart = window.Heart = function( options ) {
     this.distance = options.distance || 1;
@@ -16,19 +16,65 @@
     }
   };
 
+  transform3d = (function() {
+    var fakeBody,
+      doc = document,
+      de = doc.documentElement,
+      bod = doc.body || (function() {
+        fakeBody = doc.createElement('body');
+        return de.insertBefore( fakeBody, de.firstElementChild || de.firstChild);
+      }()),
+      el = document.createElement( "div" ),
+      prop = "transform-3d",
+      vendors = [ "Webkit", "Moz", "O" ],
+      mm = "matchMedia" in window,
+      ret = false,
+      transforms, t;
+
+    if( mm ) {
+      ret = window.matchMedia( "(-" + vendors.join( "-" + prop + "),(-" ) + "-" + prop + "),(" + prop + ")" ).matches;
+    }
+
+    if( !ret ) {
+      transforms = {
+        "MozTransform": "-moz-transform",
+        "transform": "transform"
+      };
+
+      bod.appendChild( el );
+
+      for ( t in transforms ) {
+        if ( el.style[ t ] !== undefined ) {
+          el.style[ t ] = "translate3d( 100px, 1px, 1px )";
+          ret = window.getComputedStyle( el ).getPropertyValue( transforms[ t ] );
+        }
+      }
+    }
+
+    if( fakeBody ) {
+      de.removeChild( fakeBody );
+    }
+
+    return ( !!ret && ret !== "none" );
+  }());
+
   proto = heart.prototype;
 
   proto._tick = function() {
     var newScrollLeft, head;
 
     // increment the current scroll appropriately
-    this._setScrollLeft( this.currentScrollLeft + this.distance );
+    this[ "_set" + ( transform3d ? "Slide" : "Scroll" ) + "Left" ]( this.currentScrollLeft + this.distance );
 
     // if the current scrolling value is larger than the stored width
     // for the head of the list by a small buffer, move the out of view
     // head to the tail of the list
     if( this.currentScrollLeft > this.headWidth + 20 ) {
-      this._moveHead();
+      if( raf ) {
+        requestAnimationFrame( this._moveHead );
+      } else {
+        this._moveHead();
+      }
     }
   };
 
@@ -49,14 +95,30 @@
     this.currentScrollLeft = this.element.scrollLeft = value;
   };
 
+  proto._setSlideLeft = function( value ) {
+    var curr = ( this.scrollable.style.webkitTransform || "0" ).match(/([0-9])/); //TODO: There must be a better way of doing this.
+    this.scrollable.style.webkitTransform = "translateX(" + -value + "px)";
+
+    this.currentScrollLeft = curr = value;
+  };
+
   proto._head = function() {
     return this.scrollable.querySelector( "p" ); // TODO: Could be any element; should probably be configurable.
   };
 
   proto.start = function() {
     var self = this;
-    this.intervalId = setInterval(function() {
-      self._tick();
-    }, this.interval );
+
+     if( raf ) {
+      beat = function() {
+        self._tick();
+        requestAnimationFrame( beat );
+      };
+      requestAnimationFrame( beat );
+    } else {
+      this.intervalId = setInterval(function() {
+        self._tick();
+      }, this.interval );
+    }
   };
 })();
