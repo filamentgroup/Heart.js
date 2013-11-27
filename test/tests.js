ffea74030ef44facd9533c3e74867fb934af2855
John Bender <john.m.bender@gmail.com>
2013-11-27 15:24:38 -0800

    assign the offset method once, test that it's assigned properly

--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,13 @@
 (function() {
-	var instance, constructor = window.Heart;
+	var oldTransformSupport,instance, constructor = window.Heart;
 
 	module( "constructor", {
 		setup: function() {
+			oldTransformSupport = constructor.prototype._transformSupport;
+		},
+
+		teardown: function() {
+			constructor.prototype._transformSupport = oldTransformSupport;
 		}
 	});
 
@@ -27,5 +32,29 @@
 			ok( instance.currentraf || instance.intervalId );
 			start();
 		}, instance.interval + 10 );
+	});
+
+	test( "transform support uses slide method", function() {
+		constructor.prototype._transformSupport = function() {
+			return true;
+		};
+
+		instance = new constructor({
+			element: document.querySelector( ".heart" )
+		});
+
+		equal( instance._setOffset, constructor.prototype._setSlideLeft );
+	});
+
+	test( "transform support uses scroll method", function() {
+		constructor.prototype._transformSupport = function() {
+			return false;
+		};
+
+		instance = new constructor({
+			element: document.querySelector( ".heart" )
+		});
+
+		equal( instance._setOffset, constructor.prototype._setScrollLeft );
 	});
 })();
