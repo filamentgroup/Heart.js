aa2dffad9fa2016dde4f17b817b97f2fab4e68d5
John Bender <john.m.bender@gmail.com>
2013-11-27 14:47:48 -0800

    negative test for start option

--- test/tests.js
+++ test/tests.js
@@ -3,15 +3,28 @@
 
 	module( "constructor", {
 		setup: function() {
-			instance = new constructor({
-				element: document.querySelector( ".heart" )
-			});
 		}
 	});
 
 	asyncTest( "start not called by default", function() {
+		instance = new constructor({
+			element: document.querySelector( ".heart" )
+		});
+
 		setTimeout(function() {
-			ok( !instance.current && !instance.intervalId );
+			ok( !instance.currentraf && !instance.intervalId );
+			start();
+		}, instance.interval + 10 );
+	});
+
+	asyncTest( "start called when the option is set", function() {
+		instance = new constructor({
+			element: document.querySelector( ".heart" ),
+			start: true
+		});
+
+		setTimeout(function() {
+			ok( instance.currentraf || instance.intervalId );
 			start();
 		}, instance.interval + 10 );
 	});
