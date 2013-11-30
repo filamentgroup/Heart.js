30a600ae4c89012c475875d12585fcd97eac9b86
Jeffrey Lembeck <jlembeck@gmail.com>
2013-11-29 21:07:31 -0800

    Make this work in IE9

--- customEvent.js
+++ customEvent.js
@@ -2,19 +2,17 @@
 (function ( w ) {
 	"use strict";
 
-	if( !("CustomEvent" in w ) ){
-		var doc = w.document;
+	var doc = w.document;
 
-		var CustomEvent = function( e, params ) {
-			params = params || { bubbles: false, cancelable: false, detail: undefined };
-			var evt;
-			evt = doc.createEvent( 'CustomEvent' );
-			evt.initCustomEvent( e, params.bubbles, params.cancelable, params.detail );
-			return evt;
-		};
+	var CustomEvent = function( e, params ) {
+		params = params || { bubbles: false, cancelable: false, detail: undefined };
+		var evt = doc.createEvent( 'CustomEvent' );
+		evt.initCustomEvent( e, params.bubbles, params.cancelable, params.detail );
+		return evt;
+	};
 
-		CustomEvent.prototype = w.CustomEvent.prototype;
+	CustomEvent.prototype = w.CustomEvent.prototype;
 
-		w.CustomEvent = CustomEvent;
-	}
+	w.CustomEvent = CustomEvent;
+
 }(this));
