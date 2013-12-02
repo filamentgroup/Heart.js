348a1eb45dfb0276cdccd5e12cde19365d8faccb
Jeffrey Lembeck <jlembeck@gmail.com>
2013-12-02 06:05:58 -0800

    Test for support of the constructor so we don't overwrite for Chrome/Firefox/Alrady Supporting Browsers

--- customEvent.js
+++ customEvent.js
@@ -2,17 +2,21 @@
 (function ( w ) {
 	"use strict";
 
-	var doc = w.document;
+	try {
+		var c = new w.CustomEvent("t");
+	} catch(e) {
+		var doc = w.document;
 
-	var CustomEvent = function( e, params ) {
-		params = params || { bubbles: false, cancelable: false, detail: undefined };
-		var evt = doc.createEvent( 'CustomEvent' );
-		evt.initCustomEvent( e, params.bubbles, params.cancelable, params.detail );
-		return evt;
-	};
+		var CustomEvent = function( e, params ) {
+			params = params || { bubbles: false, cancelable: false, detail: undefined };
+			var evt = doc.createEvent( 'CustomEvent' );
+			evt.initCustomEvent( e, params.bubbles, params.cancelable, params.detail );
+			return evt;
+		};
 
-	CustomEvent.prototype = w.CustomEvent.prototype;
+		CustomEvent.prototype = w.CustomEvent.prototype;
 
-	w.CustomEvent = CustomEvent;
+		w.CustomEvent = CustomEvent;
+	}
 
 }(this));
